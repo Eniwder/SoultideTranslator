ee6b3e86e67a8ef857343accaa35269a18799dc0
Eniwder <akabudou0120@yahoo.co.jp>
2023-07-20 20:07:09 +0900

    init

---
+++ src/App.vue
@@ -0,0 +1,115 @@
+<template>
+  <v-app>
+    <v-app-bar color="grey-darken-4" title="Soultide Tarnslator" :elevation="0" density="compact">
+      <template v-slot:append>
+        <v-btn icon=" mdi-github" href="https://github.com/Eniwder/SoultideTranslator" target="_blank"></v-btn>
+      </template>
+    </v-app-bar>
+    <v-main>
+      <div>
+        <p>Soultide</p>
+        <v-textarea v-model="text" class="lang-st"></v-textarea>
+        <SimpleKeyboard @onChange="onVChange" :input="text" />
+      </div>
+      <div>
+        <img src="@/assets/arrow.png" class="arrow">
+        <p>English</p>
+        <v-textarea v-model="text" class="lang-en"></v-textarea>
+      </div>
+    </v-main>
+  </v-app>
+</template>
+
+<script setup>
+import { ref, watch, onMounted } from 'vue';
+import SimpleKeyboard from "./components/SimpleKeyboards.vue";
+const text = ref("")
+
+function onVChange(input) {
+  text.value = input;
+}
+
+onMounted(() => {
+  const _text = window.location.pathname.substring(1);
+  if (_text !== "") {
+    text.value = _text.replace('%0A', '\n');
+  }
+})
+
+watch(text, () => {
+  window.history.replaceState(null, '', `/${text.value.replace('\n', '%0A')}`);
+})
+
+
+
+</script>
+
+<style>
+main {
+  background: linear-gradient(215deg, #616161 29.9%, #386c8f 80%), linear-gradient(304deg, rgba(63, 91, 78, 0.4) 24.9%, #4d363d 91%), linear-gradient(10deg, #1d2f3b 19.9%, #3d4449 50%), linear-gradient(145deg, #5a9c8e 15.9%, #9c8994 40%);
+  flex-grow: 1;
+  padding: 2em;
+  text-align: center;
+  background-blend-mode: hard-light;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  --v-layout-top: 64px !important;
+  --v-layout-left: 24px !important;
+  --v-layout-right: 24px !important;
+}
+
+img {
+  display: flex;
+}
+
+@font-face {
+  font-family: 'Soultide';
+  src: url('@/assets/mySoultideFont.ttf') format('truetype');
+}
+
+p {
+  font-family: Arial, Helvetica, sans-serif;
+  font-size: 16px;
+  position: relative;
+  color: lightgray;
+  top: 32px;
+  left: -46%;
+  z-index: 2;
+  user-select: none;
+}
+
+.lang-st {
+  font-family: Soultide;
+}
+
+.lang-en {
+  font-family: 'IBM Plex Sans', Arial, Helvetica, sans-serif;
+}
+
+.lang-st textarea {
+  font-size: 28px;
+}
+
+.lang-en textarea {
+  font-size: 32px;
+}
+
+.lang-st textarea,
+.lang-en textarea {
+  background-color: #19191c;
+  color: white;
+  padding-top: 48px;
+  padding-left: 24px;
+}
+
+.arrow {
+  margin-top: 12px;
+  left: 50%;
+  position: relative;
+}
+
+.v-textarea .v-field__input {
+  -webkit-mask-image: none !important;
+}
+</style>
---
+++ src/components/SimpleKeyboards.vue
@@ -0,0 +1,82 @@
+<template>
+  <div :class="keyboardClass"></div>
+</template>
+
+<script>
+import Keyboard from "simple-keyboard";
+import "simple-keyboard/build/css/index.css";
+
+export default {
+  name: "SimpleKeyboard",
+  props: {
+    keyboardClass: {
+      default: "simple-keyboard",
+      type: String
+    },
+    input: {
+      type: String
+    }
+  },
+  data: () => ({
+    keyboard: null
+  }),
+  mounted() {
+    this.keyboard = new Keyboard(this.keyboardClass, {
+      onChange: this.onChange,
+      onKeyPress: this.onKeyPress,
+      newLineOnEnter: true,
+      layout: {
+        default: [
+          "q w e r t y u i o p {bksp}",
+          "a s d f g h j k l {enter}",
+          "z x c v b n m",
+          "{space}"
+        ]
+      }
+    });
+  },
+  methods: {
+    onChange(input) {
+      this.$emit("onChange", input);
+    },
+    onKeyPress(button) {
+      this.$emit("onKeyPress", button);
+
+      /**
+       * If you want to handle the shift and caps lock buttons
+       */
+      if (button === "{shift}" || button === "{lock}") this.handleShift();
+    },
+  },
+  watch: {
+    input(input) {
+      this.keyboard.setInput(input);
+    }
+  }
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+div {
+  font-family: Soultide;
+}
+</style>
+<style>
+.hg-button-bksp,
+.hg-button-enter {
+  font-family: Arial, Helvetica, sans-serif
+}
+
+.hg-button {
+  color: white;
+}
+
+.hg-theme-default {
+  background-color: #6d6d6d;
+}
+
+.hg-theme-default .hg-button {
+  background-color: #19191c;
+}
+</style>
